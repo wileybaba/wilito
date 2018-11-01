089ee6b9f2fcf17bf4f67f2046c30968bf410b87
wileybaba <wileymckayconte@gmail.com>
2018-11-01 13:18:55 -0600

    rebuilding site Thu Nov  1 13:18:55 MDT 2018

--- deploy.sh
+++ deploy.sh
@@ -3,12 +3,12 @@
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 # Build the project.
-hugo 
+hugo
 
 # Go To Public folder
 cd public
 # Add changes to git.
-git add .
+git add -A
 
 # Commit changes.
 msg="rebuilding site `date`"
